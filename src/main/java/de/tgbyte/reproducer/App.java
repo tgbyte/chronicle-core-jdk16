322df8ab6707c5367ce14facd91a192a415ab227
Thilo-Alexander Ginkel <tg@tgbyte.de>
2021-03-19 20:10:53 +0100

    Initial import

---
+++ src/main/java/de/tgbyte/reproducer/App.java
@@ -0,0 +1,12 @@
+package de.tgbyte.reproducer;
+
+import net.openhft.chronicle.core.Jvm;
+
+public class App {
+    static {
+        Jvm.init();
+    }
+
+    public static void main(String[] args) {
+    }
+}
